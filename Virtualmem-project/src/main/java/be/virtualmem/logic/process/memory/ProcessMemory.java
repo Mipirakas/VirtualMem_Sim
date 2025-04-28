9205da366a9625d9d43f73a8afaed8e376c1ef2a
Anthony Tacquet <anthony.tacquet@student.odisee.be>
2025-04-28 20:51:01 +0200

    Progress of the day

--- Virtualmem-project/src/main/java/be/virtualmem/logic/process/memory/ProcessMemory.java
+++ Virtualmem-project/src/main/java/be/virtualmem/logic/process/memory/ProcessMemory.java
@@ -2,6 +2,7 @@ package be.virtualmem.logic.process.memory;
 
 import be.virtualmem.global.Constants;
 import be.virtualmem.global.address.IAddress;
+import be.virtualmem.logic.process.memory.entry.PageTableEntry;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,15 +20,38 @@ public class ProcessMemory {
     public void read(IAddress address) {
         // If memory not mapped yet, end process (Segmentation fault)
         // If memory is mapped, but not in physical memory, reallocate
+        PageTableEntry pageTableEntry = pageTableStructure.getPageTableEntry(address);
+        if (pageTableEntry != null) {
+            try {
+                pageTableEntry.read();
+            } catch (Exception e) { // Make custom exception
+                e.printStackTrace();
+            }
+        }
     }
 
     public void write(IAddress address) {
         // If memory not mapped yet, end process (Segmentation fault)
         // If memory is mapped, but not in physical memory, reallocate
+        PageTableEntry pageTableEntry = pageTableStructure.getPageTableEntry(address);
+        if (pageTableEntry != null) {
+            try {
+                pageTableEntry.write();
+            } catch (Exception e) { // Make custom exception
+                e.printStackTrace();
+            }
+        }
     }
 
     public void map(IAddress address, int size) {
-
+        PageTableEntry pageTableEntry = pageTableStructure.getPageTableEntry(address);
+        if (pageTableEntry != null) {
+            try {
+                pageTableEntry.read();
+            } catch (Exception e) { // Make custom exception
+                e.printStackTrace();
+            }
+        }
     }
 
     public void unmap(IAddress address, int size) {
